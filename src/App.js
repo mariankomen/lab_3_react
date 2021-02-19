29dd3d2af263e0db534774ef5715607d7403d2ad
Marian Lyzhychka <52856608+mariankomen@users.noreply.github.com>
2021-02-19 12:48:42 +0200

    'final'

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [line, setLine] = useState(0.0001)
+    const [color, setColor] = useState('blue')
+    const [wid, setWid] = useState(1)
+    const [polz, setPolz] = useState(1)
+
+
+    //Побудова самої параболи через canvas
+    const canvas = document.getElementById('canvas') //Гетаєм блок канваса який в нас в index.html файлі
+    let ctx = canvas.getContext("2d");
+    for (let i = -20; i < 20; i += line) {
+        ctx.fillStyle = color;
+        ctx.fillRect(250 + i * 10, 250 - Math.pow(i, 2), wid, polz);
+    }
+    //
+
+    return (
+        <div className="App">
+            <h1>Lab 3 Lyzhychka Marian TP-44 :D</h1>
+            <button
+                onClick={(e) => {
+                    setLine(0.0001)
+                    setWid(1)
+                    setColor('blue')
+                    e.preventDefault()
+                }}
+            >Змінити на суцільну
+            </button>
+            <button
+                onClick={(e) => {
+                    setLine(1)
+                    setColor('yellow')
+                    e.preventDefault()
+                }}
+            >Змінити на переривчасту
+            </button>
+            <button
+                onClick={() => setColor('black')}
+            >Чорн
+            </button>
+            <p>Змінити товщину лінії: </p>
+            <input id="typeinp"
+                   type="range"
+                   min="0"
+                   max="25"
+                   value={polz}
+                   step="1"
+                   onChange={(e) =>setPolz(e.target.value)}
+            />
+        </div>
+    );
 }
 
 export default App;
